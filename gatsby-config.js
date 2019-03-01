05128b0c8f8d3ebe0d65243619ad32956422a9b9
Adam Garrett-Harris <agarrharr@users.noreply.github.com>
2019-02-28 17:23:43 -0700

    Update to using gatsby-plugin-compile-es6-packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
   __experimentalThemes: [
     {
-      resolve: 'gatsby-theme-paprika',
+      resolve: `gatsby-theme-paprika`,
       options: {},
+    },
+  ],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-compile-es6-packages`,
+      options: {
+        modules: [`gatsby-theme-paprika`],
+      },
     },
   ],
 };
